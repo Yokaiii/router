8663627342f67cdcfc6f139d8da2e039e8fca83e
JamesDevlin5 <devlin.ja@husky.neu.edu>
2019-02-01 20:03:27 -0500

    Add files via upload
    Anotha one

---
+++ SimpleTable.java
@@ -0,0 +1,79 @@
+package routing;
+
+import jdk.nashorn.api.scripting.*;
+import org.json.*;
+import sun.security.x509.*;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * A simple class to maintain the forwarding table of a router
+ */
+public class SimpleTable implements ForwardingTable {
+    
+    // A list of the entries in this table
+    List<ForwardingEntry> entries;
+    
+    /**
+     * Constructs a new Forwarding Table, and initializes the entries list
+     */
+    public SimpleTable() {
+        this.entries = new ArrayList<>();
+    }
+    
+    @Override
+    public void coalesce() {
+    for (ForwardingEntry entry1 : this.entries) {
+        for (ForwardingEntry entry2 : this.entries) {
+            // Skip the coalesce if these are the same entry
+            if (entry1.equals(entry2)) {
+                continue;
+            }
+            
+        }
+    }
+    }
+    
+    @Override
+    public void addEntry(ForwardingEntry entry) {
+        this.entries.add(entry);
+        this.coalesce();
+    }
+    
+    @Override
+    public List<ForwardingEntry> lookupRoutes(byte[] destination) {
+        return null;
+    }
+    
+    @Override
+    public JSObject dump() {
+        JSONArray  array = new JSONArray();
+        JSONObject dump  = new JSONObject();
+    
+        for (ForwardingEntry fe : entries) {
+        
+            JSONObject    json    = new JSONObject();
+            IPAddressName network = null;
+            IPAddressName netmask = null;
+        
+            try {
+                network = new IPAddressName(fe.getNetworkPrefix());
+                netmask = new IPAddressName(fe.getNetmask());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        
+            json.put("network", network.toString());
+            json.put("netmask", netmask.toString());
+        
+            json.put("peer", fe.getPort().getRelation());
+        
+            array.put(json);
+        }
+    
+        dump.put("msg", array);
+    
+        return (JSObject) dump;
+    }
+}
